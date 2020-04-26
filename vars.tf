67bbcc7dd6ac78cecb898f840377eedadbde34c0
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-04-26 18:35:50 -0500

    ecs

---
+++ vars.tf
@@ -0,0 +1,24 @@
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "~/.ssh/id_rsa"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "ECS_INSTANCE_TYPE" {
+  default = "t2.micro"
+}
+
+variable "ECS_AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-1924770e"
+    us-west-2 = "ami-56ed4936"
+    eu-west-1 = "ami-c8337dbb"
+  }
+}
